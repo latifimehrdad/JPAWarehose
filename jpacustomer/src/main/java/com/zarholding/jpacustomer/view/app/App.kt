81319481696e40be85f31e7affee3bf18542a5af
m-latifi <m3.latifi@gmail.com>
2023-05-02 18:33:47 +0430

    شروع مشتری

---
+++ jpacustomer/src/main/java/com/zarholding/jpacustomer/view/app/App.kt
@@ -0,0 +1,13 @@
+package com.zarholding.jpacustomer.view.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * create by m-latifi on 5/2/2023
+ */
+
+@HiltAndroidApp
+class App: Application() {
+}
