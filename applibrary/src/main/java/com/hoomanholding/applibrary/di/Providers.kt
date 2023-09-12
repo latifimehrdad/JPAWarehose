b6ed3a971276325b97d3fcb6c856c4696404b744
zar <m3.latifi@gmail.com>
2023-09-12 12:42:25 +0330

    اضافه کردن دسترسی به مشتریان

--- applibrary/src/main/java/com/hoomanholding/applibrary/di/Providers.kt
+++ applibrary/src/main/java/com/hoomanholding/applibrary/di/Providers.kt
@@ -20,10 +20,10 @@ import javax.inject.Singleton
 class Providers {
 
     companion object {
-//        var url = "http://api.hoomanholding.com:9090" // Jpa
+        var url = "http://api.hoomanholding.com:9090" // Jpa
 //        var url = "http://10.252.37.164:7575" //Zar
 //        var url = "http://h-azad-vdi:7575" //Zar
-        var url = "http://192.168.207.248:8585" //Zar
+//        var url = "http://192.168.207.248:8585" //Zar
     }
 
     //---------------------------------------------------------------------------------------------- provideUrl
