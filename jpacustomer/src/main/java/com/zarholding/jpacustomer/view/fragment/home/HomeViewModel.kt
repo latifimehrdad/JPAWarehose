296b46dc5b1a584c118a91e45589df39c1458d21
zar <m-latifi@zarmacaron.com>
2025-01-20 12:09:08 +0330

    باگ speech to text

--- jpacustomer/src/main/java/com/zarholding/jpacustomer/view/fragment/home/HomeViewModel.kt
+++ jpacustomer/src/main/java/com/zarholding/jpacustomer/view/fragment/home/HomeViewModel.kt
@@ -115,7 +115,7 @@ class HomeViewModel @Inject constructor(
     private fun newFeature(): String {
         var feature = "امکانات و تغییرات نسخه ${deviceManager.appVersionName()}"
         feature += System.getProperty("line.separator")
-        feature += "رفع برخی مشکلات فنی"
+        feature += "امکان جستجوی کالا از طریق گفتار"
 
         return feature
     }
