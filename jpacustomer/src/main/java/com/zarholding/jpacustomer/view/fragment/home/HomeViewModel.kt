adbabab622d07945aa29bc7def531ab12fc1d334
zar <m-latifi@zarmacaron.com>
2025-03-17 11:22:14 +0330

    اضافه کردن کنترل اسکرول در برنامه مشتری

--- jpacustomer/src/main/java/com/zarholding/jpacustomer/view/fragment/home/HomeViewModel.kt
+++ jpacustomer/src/main/java/com/zarholding/jpacustomer/view/fragment/home/HomeViewModel.kt
@@ -115,7 +115,7 @@ class HomeViewModel @Inject constructor(
     private fun newFeature(): String {
         var feature = "امکانات و تغییرات نسخه ${deviceManager.appVersionName()}"
         feature += System.getProperty("line.separator")
-        feature += "امکان جستجوی کالا از طریق گفتار"
+        feature += "کنترل اسکرول در دفتر مشتری"
 
         return feature
     }
